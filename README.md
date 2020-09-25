162a2bd48fbe8bef9560e598da1a99c24ba75011
iparrales123 <47342722+iparrales123@users.noreply.github.com>
2020-09-25 12:13:30 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# CoffeeRun
+ 
---
+++ scripts/truck.js
@@ -0,0 +1,66 @@
+
+///////////////////////////////ORIGINAL CODE IS BELOW COMMENTED THIS IS JUST A TEST////////////////////////////////////////////////////////////
+(function (window){
+    'use strict';
+    var App = window.App || {};
+
+    class Truck {
+        constructor(truckId, db){
+            this.truckId = truckId;
+            this.db = db;
+        }
+        createOrder(order) {
+            console.log('Adding order for ' + order.emailAddress);
+            this.db.add(order.emailAddress, order);
+        }
+
+        deliverOrder(customerId){
+            console.log('Delivering order for ' + customerId);
+            this.db.remove(customerId);
+        }
+
+        printOrders(){
+            var customerIdArray = Object.keys(this.db.getAll());
+
+            console.log('Truck #' + this.truckId + ' has pending orders:');
+            customerIdArray.forEach(function (id) {
+                console.log(this.db.get(id));
+            }.bind(this));
+        }
+    }
+
+    App.Truck = Truck;
+    window.App = App;
+}) (window);
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// (function (window){
+//     'use strict';
+//     var App = window.App || {};
+
+//     function Truck(truckId, db) {
+//         this.truckId = truckId;
+//         this.db = db;
+//     }
+
+//     Truck.prototype.createOrder = function (order) {
+//         console.log('Addind order for ' + order.emailAddress);
+//         this.db.add(order.emailAddress, order);
+//     };
+
+//     Truck.prototype.deliverOrder = function (customerId) {
+//         console.log('Delivering order for ' + customerId);
+//         this.db.remove(customerId);
+//     };
+
+//     Truck.prototype.printOrders = function () {
+//         var customerIdArray = Object.keys(this.db.getAll());
+
+//         console.log('Truck #' + this.truckId + ' has pending orders:');
+//         customerIdArray.forEach(function (id) {
+//             console.log(this.db.get(id));
+//         }.bind(this));
+//     };
+
+//     App.Truck = Truck;
+//     window.App = App;
+// }) (window);
